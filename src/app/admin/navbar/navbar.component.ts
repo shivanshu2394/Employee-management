491857b733d899ce0862c4cfb0ee2026f6af2e20
prashant joshi <joshi111prashant@gmail.com>
2021-10-08 17:46:41 +0530

    testing commit

--- src/app/admin/navbar/navbar.component.ts
+++ src/app/admin/navbar/navbar.component.ts
@@ -14,7 +14,7 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
     this.adminUserDetail=this.dataService.setData()
-    this.dataService.getData(this.adminUserDetail)
+    this.dataService.getData(this.adminUserDetail);
   }
 
 }
